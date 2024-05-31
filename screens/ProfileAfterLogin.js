d25a26f755843afca554fa83d23c3f889e984fb8
Regine <18221025@std.stei.itb.ac.id>
2024-05-31 08:48:43 +0700

    Edit Profile After Login

--- screens/ProfileAfterLogin.js
+++ screens/ProfileAfterLogin.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Profile = () => {
+const ProfileScreen = () => {
   return (
-    <SafeAreaView className="flex justify-center items-center px-[9px]">
-      <View className="h-full w-full">
-        <View>
-            <Text className="text-[30px] font-bold text-center">Profile</Text>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.closeButton}>
+          <MaterialCommunityIcons name="close" size={24} color="#000" />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Profile</Text>
       </View>
-    </SafeAreaView>
+      <View style={styles.profileImageContainer}>
+        <MaterialCommunityIcons name="account" size={100} color="#ccc" />
+      </View>
+      <Text style={styles.profileName}>Regine Fidellia H</Text>
+      <Text style={styles.joinDate}>Join since April 2024</Text>
+      <TouchableOpacity style={styles.editProfileButton}>
+        <Text style={styles.editProfileText}>Edit profile</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
-export default Profile;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    paddingTop: 50,
+  },
+  header: {
+    width: '90%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  closeButton: {
+    position: 'absolute',
+    left: 0,
+  },
+  headerTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    flex: 1,
+    marginTop: -3
+  },
+  profileImageContainer: {
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  profileName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  joinDate: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  editProfileButton: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+  },
+  editProfileText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default ProfileScreen;
